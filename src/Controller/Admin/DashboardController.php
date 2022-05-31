2bfb6f5959a8308426fdb230552917cc52d5d409
krovean <krovean.web.develop@gmail.com>
2022-06-01 00:25:01 +0300

    new_fitures

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,12 +6,18 @@ use App\Entity\Answer;
 use App\Entity\Question;
 use App\Entity\Topic;
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -19,21 +25,58 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-         return $this->render('admin/index.html.twig');
+        return $this->render('admin/index.html.twig');
     }
 
+    /**
+     * @return Dashboard
+     */
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
             ->setTitle('Cauldron Overflow Admin');
     }
 
+    /**
+     * @param UserInterface $user
+     * @return UserMenu
+     * @throws Exception
+     */
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        if (!$user instanceof User) {
+            throw new Exception('Wrong User');
+        }
+
+        return parent::configureUserMenu($user)
+            ->setAvatarUrl($user->getAvatarUrl())
+            ->setMenuItems(
+                [
+                    MenuItem::linkToUrl('My Profile', 'fa fa-user', $this->generateUrl('app_profile_show'))
+                ]
+            );
+    }
+
+
+    /**
+     * @return iterable
+     */
     public function configureMenuItems(): iterable
     {
+        yield MenuItem::linkToUrl('Homepage', 'fa fa-home', $this->generateUrl('app_homepage'));
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-dashboard');
         yield MenuItem::linkToCrud('Questions', 'fa fa-question-circle', Question::class);
         yield MenuItem::linkToCrud('Answers', 'fa fa-comments', Answer::class);
         yield MenuItem::linkToCrud('Topics', 'fa fa-folder', Topic::class);
         yield MenuItem::linkToCrud('Users', 'fa fa-users', User::class);
+    }
+
+    /**
+     * @return Actions
+     */
+    public function configureActions(): Actions
+    {
+        return parent::configureActions()
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
     }
 }
--- src/Controller/Admin/TopicCrudController.php
+++ src/Controller/Admin/TopicCrudController.php
@@ -3,23 +3,27 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Topic;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class TopicCrudController extends AbstractCrudController
 {
+    /**
+     * @return string
+     */
     public static function getEntityFqcn(): string
     {
         return Topic::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
+    /**
+     * @param Actions $actions
+     * @return Actions
+     */
+    public function configureActions(Actions $actions): Actions
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        return parent::configureActions($actions)
+            ->disable(Action::DETAIL);
     }
-    */
 }
