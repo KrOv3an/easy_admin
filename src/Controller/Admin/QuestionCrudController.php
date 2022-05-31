bb750fa4e38e787af60cee9b8fd844cd7bde3ddc
krovean <krovean.web.develop@gmail.com>
2022-05-31 23:59:08 +0300

    cruds

---
+++ src/Controller/Admin/QuestionCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Question;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class QuestionCrudController extends AbstractCrudController
+{
+    /**
+     * @return string
+     */
+    public static function getEntityFqcn(): string
+    {
+        return Question::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
